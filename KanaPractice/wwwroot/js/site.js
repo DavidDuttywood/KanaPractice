a777aed8e19bfa28cfb007fe9926c9881ae7fe2b
David Woodward <david.owib@gmail.com>
2021-05-11 19:39:28 +0100

    added motivational sushi mascot into the game

--- KanaPractice/wwwroot/js/site.js
+++ KanaPractice/wwwroot/js/site.js
@@ -1,4 +1,18 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
 
-// Write your JavaScript code.
+    let score = parseInt($("#score").text());
+
+    let messageBank = [
+        score + " is good, but i KNOW you can do better!",
+        "Anything over " + score + " is just showing off!",
+        "There is no way you got " + score + " without cheating!",
+        "Sugoi!, " + score + " is practically flying!"
+    ];
+
+    if (score % 5 == 0 && score !== 0) {
+        
+        $("#motivationalMessageContent").text(messageBank[Math.floor(Math.random() * 4)]);
+
+        $("#motivationalMessage").fadeIn();
+    }
+});
